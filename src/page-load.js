cb8c686184933ab64256727efe8fa72ab851ab2d
KailasMetcalfe <kailasmetz@gmail.com>
2026-07-15 17:14:21 +0100

    Change to initiate page content via JS instead of HTML

---
+++ src/page-load.js
@@ -0,0 +1,24 @@
+import pizzaImg from "./pizza.jpg";
+
+export default function loadPageDefault() {
+    const container = document.querySelector("#content");
+
+    const image = document.createElement("img");
+    image.src = pizzaImg;
+    image.alt = "Pizza on bench with beer"
+
+    const div = document.createElement("div");
+
+    const headline = document.createElement("h1");
+    headline.textContent = "A Taste of Italy In Bristol";
+
+    const tagline = document.createElement("p");
+    tagline.textContent = "We welcome you to ... , where passion meets comfort. \
+    Try our range of delicious pizzas and pastas for an affordable price.";
+
+    div.appendChild(headline);
+    div.appendChild(tagline);
+
+    container.appendChild(image);
+    container.appendChild(div);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,8 @@ export default {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/i,
-        use: ["html-loader"],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
